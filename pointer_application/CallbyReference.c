6ff7dfb0a05edcc09e5885de5d61f2ab23d7f72f
런닝머신뛰는 하연이 <59410853+HAMA-coder@users.noreply.github.com>
2020-05-15 18:43:40 +0900

    CallbyValue & CallbyReference
    함수는 순서대로, 반환형, 함수이름, 매개변수 선언문 으로 작성된다.
    CallbyValue 함수에서는 count를 증가시켰지만 main 함수에서 
    우리가 원하는 결과값을 얻지 못했다. 왜 그런 것일까?
    그것은 바로, 함수문을 빠져나가면서 그 값이 소멸되었기 때문이다.
    
    반대로 CallbyReference는 주소값에 접근하면서
    변경된 값이 유지되었기에 원하는 결과값을 얻을 수 있는 것이다.

---
+++ pointer_application/CallbyReference.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+void CallbyVal(int i,int count); // count를 1씩 증가
+void CallbyRef(int i,int* pcount); 
+int main(void){
+    int count=0;
+    int* pcount=&count;
+  
+  printf("[main] value of count: %d ,address of count:%ld\n",count,&count);
+  // CallbyValue 함수를 쓴 경우
+  printf("Using CallbyVal\n");
+  for(int i=0;i<10;i++){
+      CallbyVal(i,count);
+      if(count==3)break;
+  }
+  printf("[main] value of count: %d ,address of count:%ld\n",count,&count);
+
+
+  // CallbyReference 를 쓴 경우
+  for(int i=0;i<10;i++){
+      CallbyRef(i,pcount);
+      if(count==3)break;
+  }
+  printf("[main] value of count: %d ,address of count:%ld\n",*pcount,pcount);
+  //
+  return 0;
+}
+
+void CallbyVal(int i,int count){
+    count++;
+    printf("\t[%2dth CallbyVal] Valude of count : %d,Address of count:%ld\n",i,count,&count);
+}
+void CallbyRef(int i,int* pcount){
+    (*pcount)++;
+    printf("\t[%2dth CallbyVal] Valude of count : %d,Address of count:%ld\n",i,*pcount,pcount;)
+}
